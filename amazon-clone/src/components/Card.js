9735f34610c8f71e88a491589ac7481e555a8a66
Ahmed Mostafa <ahmed-mostafa2020>
2023-08-12 19:37:15 +0300

    all project done still style1

--- amazon-clone/src/components/Card.js
+++ amazon-clone/src/components/Card.js
@@ -1,12 +1,36 @@
 import starIcon from "../images/icons/star.png";
-import { AiOutlineShoppingCart } from "react-icons/ai";
+import {
+  AiFillHeart,
+  AiOutlineHeart,
+  AiOutlineShoppingCart,
+} from "react-icons/ai";
 import { useAuth } from "../context/GlobalState";
+import { useState } from "react";
 
 const Card = ({ id, image, title, rating, reviews, prevPrice, newPrice }) => {
+  const [disabled, setDisabled] = useState(false);
+  const [clicked, setClicked] = useState(false);
+
   const { dispatch } = useAuth();
 
-  const addToBasket = (e) => {
+  const addToFavorite = (e) => {
     e.preventDefault();
+    setClicked(true);
+    setDisabled(true);
+
+    dispatch({
+      type: "ADD_TO_FAVORITES",
+      item: {
+        id: id,
+        title: title,
+        price: newPrice,
+        image: image,
+        rating: rating,
+      },
+    });
+  };
+
+  const addToBasket = () => {
     dispatch({
       type: "ADD_TO_BASKET",
       item: {
@@ -19,9 +43,25 @@ const Card = ({ id, image, title, rating, reviews, prevPrice, newPrice }) => {
     });
   };
 
+  console.log(clicked);
+
   return (
     <>
       <section className="card">
+        <div className="favorite">
+          <button
+            className="heart-container"
+            onClick={addToFavorite}
+            disabled={disabled}
+          >
+            {clicked ? (
+              <AiFillHeart className="favorite-icon" />
+            ) : (
+              <AiOutlineHeart className="favorite-icon" />
+            )}
+          </button>
+        </div>
+
         <img src={image} alt={title} className="card-img" />
         <div className="card-details">
           <h3 className="card-title">{title}</h3>
